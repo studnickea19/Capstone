6bc08ee323fffaf30c9163b98a6e3ecc8a420455
Ellie Studnicka <e.studnicka@gmail.com>
2018-05-17 11:58:51 -0500

    Created project, implemented Sass and working on custom styles

---
+++ BestFriendFinder2/BestFriendFinder2/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BestFriendFinder2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Best Friend Finder is your single source for all things dog.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Tell us what you think.";
+
+            return View();
+        }
+    }
+}
---
+++ BestFriendFinder2/BestFriendFinder2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BestFriendFinder2.MvcApplication" Language="C#" %>
